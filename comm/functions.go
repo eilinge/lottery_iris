f7b0314a133350170bcdb944f2ec5212c7af3441
一凡Sir <yz124@163.com>
2019-08-30 17:19:19 +0800

    Ip4toInt

--- comm/functions.go
+++ comm/functions.go
@@ -137,9 +137,9 @@ func Ip4toInt(ip string) int64 {
 	bits := strings.Split(ip, ".")
 	if len(bits) == 4 {
 		b0, _ := strconv.Atoi(bits[0])
-		b1, _ := strconv.Atoi(bits[0])
-		b2, _ := strconv.Atoi(bits[0])
-		b3, _ := strconv.Atoi(bits[0])
+		b1, _ := strconv.Atoi(bits[1])
+		b2, _ := strconv.Atoi(bits[2])
+		b3, _ := strconv.Atoi(bits[3])
 		var sum int64
 		sum += int64(b0) << 24
 		sum += int64(b1) << 16

1d2d2aea4248f4ea4502581ed70f9d8a32f183fd
吴长青 <17601329166@163.com>
2020-03-31 18:16:47 +0800

    update conf/db.go.

--- conf/db.go
+++ conf/db.go
@@ -17,7 +17,7 @@ type DbConfig struct {
 // 系统中所有mysql主库 root:root@tcp(127.0.0.1:3306)/lottery?charset=utf-8
 var DbMasterList = []DbConfig{
 	{
-		Host:      "47.101.187.227",
+		Host:      "127.0.0.1",
 		Port:      3306,
 		User:      "root",
 		Pwd:       "eilinge",
